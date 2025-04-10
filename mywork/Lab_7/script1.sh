5a46807730b6d0e59d0213679c123ca49ac28644
Karan Rawat <134340336+KaranRawatUVA@users.noreply.github.com>
2025-04-10 17:54:12 -0400

    made it presign

--- mywork/Lab_7/script1.sh
+++ mywork/Lab_7/script1.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "First argument: $1"
 echo "Second argument: $2"
-echo "Third argument: $3"
+echo "Third argument: ${3:-604800}"
+
 
 
 file_name=$1
 bucket_name=$2
-length=$3
+length=${3:-604800}
 
 
 aws s3 cp $file_name s3://$bucket_name/
