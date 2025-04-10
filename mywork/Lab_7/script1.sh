1ad1e6161f49ac01f6ab2167a758a548530c8060
Karan Rawat <134340336+KaranRawatUVA@users.noreply.github.com>
2025-04-10 17:18:10 -0400

    first script

---
+++ mywork/Lab_7/script1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "First argument: $1"
+echo "Second argument: $2"
+echo "Third argument: $3"
+
+
+file_name=$1
+bucket_name=$2
+length=$3
+
+
+aws s3 cp $file_name s3://$bucket_name/
+
+
+aws s3 presign --expires-in $length s3://$bucket_name/$file_name
+
+
